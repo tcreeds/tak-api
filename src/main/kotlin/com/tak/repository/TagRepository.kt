a2865b6a59402183af6873e3d3aaaf475f0385c7
tcreeds <tcreeds@gmail.com>
2018-03-27 07:36:28 -0400

    Add liquibase

--- src/main/kotlin/com/tak/repository/TagRepository.kt
+++ src/main/kotlin/com/tak/repository/TagRepository.kt
@@ -2,8 +2,9 @@ package com.tak.repository
 
 import com.tak.repository.entity.TagEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
-
+@Repository
 interface TagRepository : CrudRepository<TagEntity, String> {
     fun findByTaskId(id: String): List<TagEntity>
     fun findByValue(value: String): List<TagEntity>
