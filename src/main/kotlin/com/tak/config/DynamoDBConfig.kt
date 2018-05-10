ce27ed5ba0a15e2515b6db75ac506899e1bc4c78
tcreeds <tcreeds@gmail.com>
2018-05-10 14:08:15 -0400

    Update dynamodb client

--- src/main/kotlin/com/tak/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/tak/config/DynamoDBConfig.kt
@@ -1,12 +1,17 @@
 package com.tak.config
 
+import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.regions.Regions
 import org.springframework.context.annotation.Bean
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -20,21 +25,19 @@ open class DynamoDBConfig {
     private val amazonDynamoDBEndpoint: String? = null
 
     @Bean
+    @Profile("dev")
     open fun amazonDynamoDB(): AmazonDynamoDB {
-        val amazonDynamoDB = AmazonDynamoDBClient(amazonAWSCredentials())
-        amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint)
-        return amazonDynamoDB
+        return AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build()
     }
 
-    @Bean
-    @Profile("!local")
-    open fun amazonAWSCredentials(): AWSCredentials {
-        return DefaultAWSCredentialsProviderChain().credentials
-    }
-
-    @Bean
+    @Bean(name=["amazonDynamoDB"])
     @Profile("local")
-    open fun amazonAWSCredentialsLocal(): AWSCredentials {
-        return ProfileCredentialsProvider().credentials
+    open fun amazonDynamoDBLocal(): AmazonDynamoDB {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://localhost:8010", "us-east-1"))
+                .build()
     }
+
 }
